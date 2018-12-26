588e99e18605a4efbdbbf36c9b5913620f9a61a6
Verneri K <Dryicefox@users.noreply.github.com>
2018-12-26 14:09:03 -0600

    Day 1: Data Types
    https://www.hackerrank.com/challenges/30-data-types/problem

---
+++ Day 1: Data Types.py
@@ -0,0 +1,24 @@
+### Name: Verneri Kalviainen (dryicefox)
+### Date: 12/26/2018
+### Title: Day 1: Data Types
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+o = 8
+a = 4.0
+f = 'is the best place to learn and practice coding!'
+# Read and save an integer, double, and String to your variables.
+o = int(input())
+a = float(input()) ###Python is dynamically typed, so we just need to ensure that what we are receiving is a float so it can keep the property of decimal.
+f = input()
+# Print the sum of both integer variables on a new line.
+print(i+o)
+# Print the sum of the double variables on a new line.
+print(a+d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+f)
+
