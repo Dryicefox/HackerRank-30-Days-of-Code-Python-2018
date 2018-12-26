01953c87ba9e4f1dfb441a79d792e5f6e3a49771
Verneri K <Dryicefox@users.noreply.github.com>
2018-12-26 14:30:19 -0600

    Day 9: Recursion 3
    https://www.hackerrank.com/challenges/30-recursion/problem

---
+++ Day 9: Recursion 3.py
@@ -0,0 +1,30 @@
+### Name: Verneri Kalviainen (dryicefox)
+### Date: 12/26/2018
+### Title: Day 9: Recursion 3
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
