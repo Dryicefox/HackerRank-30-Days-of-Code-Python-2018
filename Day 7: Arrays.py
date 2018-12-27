565afa6e838f11e2c4eb0c72a2aa81d9e355db8b
Verneri K <Dryicefox@users.noreply.github.com>
2018-12-27 14:02:24 -0600

    Day 7: Updated for HackerRank
    Solution that HackerRank recognizes as correct.

--- Day 7: Arrays.py
+++ Day 7: Arrays.py
@@ -1,10 +1,8 @@
 ### Name: Verneri Kalviainen (dryicefox)
-### Date: 12/26/2018
+### Date: 12/27/2018
 ### Title: Day 7: Arrays
 
-##Important note, works in compiler, but not on the website as of October, 1, 2018
-
-#!/bin/python3
+##!/bin/python3
 
 import math
 import os
@@ -16,15 +14,16 @@ import sys
 
 if __name__ == '__main__':
     n = int(input())
-
     arr = list(map(int, input().rstrip().split()))
+    arr2 = arr.copy() # If we set arr2 to arr then all we have is a pointer, not a new list
+    strvar = ''
 
-    reversed_array = []
-    for i in range(n):
-        reversed_array.append[arr[n-1-i]]
+    arr_length = len(arr) # 4 in this case, used for readability
 
-    for i in range(len(reversed_array)):
-        output_string += str[reversed_array[i]] + ' '
+    for i in range(arr_length):
+        #Take the array and go backwards to copy backwards
+        arr2[arr_length-1-i] = arr[i]
 
-    print(output_string)
-
+    for i in range(arr_length):
+        strvar += (str(arr2[i]) + ' ')
+    print(strvar)
