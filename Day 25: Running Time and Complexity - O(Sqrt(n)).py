e6aa043dc733bdf0354d3bc4b924d2a17b6bf2c3
Verneri K <Dryicefox@users.noreply.github.com>
2018-12-31 17:50:39 -0600

    Day 25: Running Time and Complexity
    Day 25: Running Time and Complexity with O(Sqrt(n)) time complexity

---
+++ Day 25: Running Time and Complexity - O(Sqrt(n)).py
@@ -0,0 +1,26 @@
+### Name: Verneri Kalviainen (dryicefox)
+### Date: 12/31/2018
+### Day 25: Running Time and Complexity
+
+import math
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def isPrime(n):
+    if n <= 1:
+        return False
+    sqrt_n = math.sqrt(n)
+    if sqrt_n.is_integer():
+        return False
+    for i in range(2, int(sqrt_n+1)):
+        if n%i == 0:
+            return False
+    return True
+
+num_test_cases = int(input())
+for i in range(num_test_cases):
+    n = int(input())
+    if isPrime(n):
+        print('Prime')
+    else:
+        print('Not prime')
+
